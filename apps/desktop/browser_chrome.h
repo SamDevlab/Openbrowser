e301886a31eec350b7b04f6d93d3b95dba3f3284
Samuel Araújo <samusilvadev@gmail.com>
2026-09-07 13:16:23 -0300

    fix: include complete CefButton type in browser chrome header

--- apps/desktop/browser_chrome.h
+++ apps/desktop/browser_chrome.h
@@ -5,6 +5,7 @@
 #include "core/session/browser_session_observer.h"
 
 #include "include/cef_base.h"
+#include "include/views/cef_button.h"
 #include "include/views/cef_button_delegate.h"
 #include "include/views/cef_panel.h"
 #include "include/views/cef_textfield_delegate.h"
