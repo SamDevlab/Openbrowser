9b562650df9cbe3eedfb43025b3b09f938c63e71
Samuel Araújo <samusilvadev@gmail.com>
2026-09-07 20:41:57 -0300

    fix(ui): remove dynamic focus panel sizing

--- apps/desktop/focus_sidebar.h
+++ apps/desktop/focus_sidebar.h
@@ -38,7 +38,6 @@ public:
 
 private:
     enum class FocusAction {
-        ToggleExpanded,
         EnqueueActiveTab,
         ActivateItem,
         PromoteItem,
@@ -63,7 +62,6 @@ private:
     core::FocusSprint sprint_;
     std::shared_ptr<bool> alive_token_{std::make_shared<bool>(true)};
     bool timer_running_{false};
-    bool expanded_{false};
 
     CefRefPtr<CefPanelDelegate> panel_delegate_;
     CefRefPtr<CefPanel> panel_;
